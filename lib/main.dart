1b46d329e3ac3b3522dd85b7e3809d582044cf66
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-27 23:22:21 +0100

    introducao de imagem

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,19 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return 
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text("I Am Rich"),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Image.asset("images/diamond.png")
+          ],
+        ),
+      ),
+    );
   }
 }
